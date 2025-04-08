870e6a560623b5a4b0c36a64c72a60f0e5ff166f
bk <josiasboco@gmail.com>
2025-04-08 01:35:33 +0100

    lucide-react installé et amélioration des composants sauf la page article

--- src/app/components/ThemeProvider.jsx
+++ src/app/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext();
 
-export const ThemeProvider = ({ children }) => {
+export function ThemeProvider ({ children })  {
   const [theme, setTheme] = useState("light");
   
   useEffect(() => {
